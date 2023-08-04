19f1025cfef4a817d4e2eb044f308b04488ec735
Comnarin <Nsirinapuk@gmail.com>
2023-08-04 14:32:12 +0700

    code update

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -5,9 +5,61 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses , :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @try = 0
   end
+
+  def word_with_guesses
+    display = ''
+    @word.each_char do |c|
+      if @guesses.include? c
+        display += c
+      else
+        display += '-'
+      end
+    end
+    display
+  end
+
+  def check_win_or_lose
+    return :win if word_with_guesses == @word
+    return :lose if @try >= 7
+    return :play if @try < 7
+  end
+
+  def guess(letters)
+
+    if letters.nil? || letters.empty? || !letters.match?(/[a-zA-Z]/)
+      raise ArgumentError
+    end
+
+    letters = letters.downcase 
+    arr = letters.chars
+
+    for letter in arr do
+      @try += 1
+      if @word.include?(letter)
+        if !guesses.include?(letter)
+          @guesses += letter 
+          return true
+        else
+          return false
+        end
+      else
+        if !wrong_guesses.include?(letter)
+          @wrong_guesses += letter 
+          return true
+        else
+          return false
+        end
+      end
+    end
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
